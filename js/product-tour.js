8edecfa35f1a880e7269184a0ab094e5c497bae0
Robophil <oghenerobo@gmail.com>
2016-06-03 15:20:51 +0100

    added plugin basic settings

---
+++ js/product-tour.js
@@ -0,0 +1,10 @@
+(function ( jQuery ) {
+	jQuery.fn.extend({
+		ProductTour: function(options){
+			// These are the defaults.
+			var settings = jQuery.extend({
+				
+			}, options);
+		}
+	});
+}( jQuery ));
