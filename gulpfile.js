0d2be657404bb3c69ccbcbf625c7f3f33067c997
Theophilus Omoregbee <theo4u@ymail.com>
2016-06-30 18:59:19 +0100

    added gulp-util to help debug if there is any error with our .js files

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var uglify = require('gulp-uglify');
+var rename= require('gulp-rename');
+var gutil = require('gulp-util');//to help log any error found in our .js file 
 
 
 gulp.task('browserSync', function() {
@@ -20,14 +22,22 @@ gulp.task('build-css', function() {
     console.log("Build css");
     browserSync.reload();
 });
-gulp.task('build-js', function() {
 
-    console.log("Build js");
-    browserSync.reload();
+
+gulp.task('build-js', function() {
+    console.log("Build Our js");
+    return gulp.src(['js/product-tour.js'])
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename('product-tour.min.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('watch',['browserSync'], function(){
     gulp.watch('js/*.js', ['build-js']);
     gulp.watch('css/*.css', ['build-css']);
     gulp.watch('**/*.html', browserSync.reload);
-})
+});
