125b2053ca90ebbd0066f5a7ae68574e6cd19a45
Theophilus Omoregbee <theo4u@ymail.com>
2016-06-30 21:50:13 +0100

    buikding process done

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var rename= require('gulp-rename');
 var gutil = require('gulp-util');//to help log any error found in our .js file
 var del = require('del');
 var runSequence = require('run-sequence');
+var cssnano = require('gulp-cssnano');
 
 gulp.task('browserSync', function() {
     browserSync.init({
@@ -15,6 +16,51 @@ gulp.task('browserSync', function() {
             baseDir: './'
         }
     })
+});
+
+
+gulp.task('build', function (callback) {
+    runSequence('clean',
+      //build in parallel
+        ['build-css', 'build-js'],
+        callback
+    )
+});
+
+
+gulp.task('build-css-files', function() {
+    console.log("Build css");
+    return gulp.src(['css/product-tour.css'])
+        .pipe(cssnano().on('error', gutil.log))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(rename('product-tour.min.css'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+gulp.task('build-js-files', function() {
+    console.log("Build Our js");
+    return gulp.src(['js/product-tour.js'])
+        .pipe(uglify().on('error', gutil.log))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename('product-tour.min.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('build-js', function (callbacks) {
+    runSequence('build-js-files','clean-js',
+        callbacks
+    )
+});
+
+gulp.task('build-css', function (callbacks) {
+    runSequence('build-css-files','clean-css',
+        callbacks
+    )
 });
 
 gulp.task('clean', function () {
@@ -29,39 +75,6 @@ gulp.task('clean-css', function () {
     return del.sync('dist/css/product-tour.css');
 });
 
-
-gulp.task('build', function (callback) {
-    runSequence('clean',
-      //build in parallel
-        ['build-css', 'build-js'],
-        callback
-    )
-});
-
-
-gulp.task('build-css', function() {
-    console.log("Build css");
-    browserSync.reload();
-});
-
-gulp.task('build-js', function (callbacks) {
-    runSequence('build-js-files','clean-js',
-        callbacks
-    )
-});
-
-
-gulp.task('build-js-files', function() {
-    console.log("Build Our js");
-    return gulp.src(['js/product-tour.js'])
-        .pipe(uglify().on('error', gutil.log))
-        .pipe(gulp.dest('dist/js'))
-        .pipe(rename('product-tour.min.js'))
-        .pipe(gulp.dest('dist/js'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-});
 
 gulp.task('watch',['browserSync'], function(){
     gulp.watch('js/*.js', ['build-js']);
